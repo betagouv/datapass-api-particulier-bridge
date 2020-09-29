5f65a2411d405d9ba1f47b8c074c974c83a41747
Stanislas Bernard <bernardstanislas@gmail.com>
2020-09-29 09:07:11 +0200

    :lock: Add mutual TLS

--- bridge/controller.py
+++ bridge/controller.py
@@ -6,14 +6,6 @@ bp = Blueprint("Application subscription", __name__, url_prefix="/applications")
 
 @bp.route("/subscribe", methods=("POST",))
 def subscribe():
-    auth_token = request.headers.get("X-Gravitee-API-Key")
-
-    if not auth_token:
-        return "Forbidden", 401
-
-    if auth_token != current_app.config.get("DATA_PASS_API_KEY"):
-        return "Unauthorized", 403
-
     content = request.json
     assert "name" in content
     assert "email" in content
