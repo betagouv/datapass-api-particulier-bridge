bec6e7bf835db5722e49d8138911f9d91c70c304
Stanislas Bernard <bernardstanislas@gmail.com>
2021-01-28 17:40:38 +0100

    :sparkles: Fail fast on gravitee requests

--- bridge/gravitee_client.py
+++ bridge/gravitee_client.py
@@ -15,6 +15,7 @@ def create_application(name, hashed_token, data_pass_id):
         },
         auth=(user, password),
     )
+    r.raise_for_status()
 
     return r.json()
 
@@ -27,6 +28,7 @@ def subscribe_to_api(application_id, plan_id, api_id):
         params={"plan": plan_id, "application": application_id},
         auth=(user, password),
     )
+    r.raise_for_status()
 
     return r.json()
 
@@ -37,18 +39,19 @@ def update_dictionary(id, name, values):
     r = requests.get(
         "{}/configuration/dictionaries/{}".format(base_url, id), auth=(user, password),
     )
+    r.raise_for_status()
     properties = r.json()["properties"]
 
     requests.put(
         "{}/configuration/dictionaries/{}".format(base_url, id),
         json={"name": name, "type": "MANUAL", "properties": {**values, **properties},},
         auth=(user, password),
-    )
+    ).raise_for_status()
 
     requests.post(
         "{}/configuration/dictionaries/{}/_deploy".format(base_url, id),
         auth=(user, password),
-    )
+    ).raise_for_status()
 
 
 def create_application_metadata(application_id, name, value):
@@ -59,6 +62,7 @@ def create_application_metadata(application_id, name, value):
         json={"value": value, "name": name, "format": "string",},
         auth=(user, password),
     )
+    r.raise_for_status()
 
     return r.json()
 
@@ -72,6 +76,7 @@ def search_user_by_email(email):
         params={"q": escaped_email, "page": 1, "size": 1},
         auth=(user, password),
     )
+    r.raise_for_status()
 
     return r.json()["data"]
 
@@ -80,39 +85,33 @@ def register_user(email):
     (base_url, user, password) = _get_credentials()
     source = current_app.config["AUTHENTICATION_SOURCE"]
 
-    r = requests.post(
+    requests.post(
         "{}/users/registration".format(base_url),
         json={"email": email, "source": source, "sourceId": email,},
         auth=(user, password),
-    )
-
-    return r.json()
+    ).raise_for_status()
 
 
 def transfer_ownership(user_id, application_id):
     (base_url, user, password) = _get_credentials()
 
-    r = requests.post(
+    requests.post(
         "{}/applications/{}/members/transfer_ownership".format(
             base_url, application_id
         ),
         json={"id": user_id, "role": "OWNER"},
         auth=(user, password),
-    )
-
-    return r.json()
+    ).raise_for_status()
 
 
 def add_user_to_application(user_id, application_id):
     (base_url, user, password) = _get_credentials()
 
-    r = requests.post(
+    requests.post(
         "{}/applications/{}/members".format(base_url, application_id),
         json={"id": user_id, "role": "USER"},
         auth=(user, password),
-    )
-
-    return r.json()
+    ).raise_for_status()
 
 
 def _get_credentials():
