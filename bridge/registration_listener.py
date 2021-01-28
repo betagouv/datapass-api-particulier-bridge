84445a8fe403d846c9cc4b48b382d97a9174f6ca
Stanislas Bernard <bernardstanislas@gmail.com>
2021-01-28 15:49:31 +0100

    :sparkles: Send an email to the datapass form owner

--- bridge/registration_listener.py
+++ bridge/registration_listener.py
@@ -5,14 +5,36 @@ from flask import render_template
 def on_user_registered(sender, **kwargs):
     application_id = kwargs["application_id"]
     application_name = kwargs["application_name"]
-    contact_email = kwargs["contact_email"]
+    technical_contact_email = kwargs["technical_contact_email"]
+    functional_contact_email = kwargs["functional_contact_email"]
+    author_email = kwargs["author_email"]
     data_pass_id = kwargs["data_pass_id"]
-    html = render_template(
-        "emails/user_registered.html",
+
+    technical_contact_notification_html = render_template(
+        "emails/technical_contact_notification.html",
         application_id=application_id,
         application_name=application_name,
-        contact_email=contact_email,
+        contact_email=technical_contact_email,
+        data_pass_id=data_pass_id,
+        signupLink="https://auth.api.gouv.fr/users/sign-up?login_hint={}&force_email=true".format(
+            technical_contact_email
+        ),
+    )
+
+    author_notification_html = render_template(
+        "emails/author_notification.html",
+        application_id=application_id,
+        application_name=application_name,
+        contact_email=author_email,
         data_pass_id=data_pass_id,
     )
 
-    send_mail(contact_email, "Votre raccordement à API Particulier", html)
+    send_mail(
+        technical_contact_email,
+        "Votre raccordement à API Particulier",
+        technical_contact_notification_html,
+    )
+
+    send_mail(
+        author_email, "Votre raccordement à API Particulier", author_notification_html,
+    )
--- bridge/subscription_factory.py
+++ bridge/subscription_factory.py
@@ -8,7 +8,14 @@ subscription_signals = Namespace()
 user_registered = subscription_signals.signal("user-registered")
 
 
-def subscribe(application_name, contact_email, data_pass_id, scopes):
+def subscribe(
+    application_name,
+    owner_email,
+    technical_contact_email,
+    functional_contact_email,
+    data_pass_id,
+    scopes,
+):
     (api_key, api_key_hash) = generate_api_key()
     (dgfip_api_id, dgfip_plan_id) = _get_dgfip_references()
     (cnaf_api_id, cnaf_plan_id) = _get_cnaf_references()
@@ -39,24 +46,25 @@ def subscribe(application_name, contact_email, data_pass_id, scopes):
     # Fill the application metadata with the API Key TODO: improve this not very stealth way of providing the API Key
     client.create_application_metadata(application["id"], "API Key", api_key)
 
-    # Register the technical contact to the API Manager if they don't have an account yet
-    contact_user_id = None
-    candidates = client.search_user_by_email(contact_email)
-    if len(candidates) > 0 and candidates[0]["email"] == contact_email:
-        contact_user_id = candidates[0]["id"]
-    else:
-        contact_user = client.register_user(contact_email)
-        contact_user_id = contact_user["id"]
+    owner_id = _find_or_register_user(owner_email)
+    technical_contact_id = _find_or_register_user(technical_contact_email)
+    functional_contact_id = _find_or_register_user(functional_contact_email)
 
     # Give the application ownership to the technical user
-    client.transfer_ownership(contact_user_id, application["id"])
+    client.transfer_ownership(technical_contact_id, application["id"])
 
-    # Notify listeners that a new registration have been made, send a mail and stuff for example
+    # Add other contacts as application users
+    client.add_user_to_application(owner_id, application["id"])
+    client.add_user_to_application(functional_contact_id, application["id"])
+
+    # Notify listeners that a new registration have been made, send a mail
     user_registered.send(
         current_app._get_current_object(),
         application_id=application["id"],
         application_name=application_name,
-        contact_email=contact_email,
+        author_email=owner_email,
+        technical_contact_email=technical_contact_email,
+        functional_contact_email=functional_contact_email,
         data_pass_id=data_pass_id,
     )
 
@@ -80,3 +88,16 @@ def _get_introspect_references():
         current_app.config["INTROSPECT_API_ID"],
         current_app.config["INTROSPECT_PLAN_ID"],
     )
+
+
+def _find_or_register_user(email):
+    # Register the contact in the API Manager if they don't have an account yet
+    contact_user_id = None
+    candidates = client.search_user_by_email(email)
+    if len(candidates) > 0 and candidates[0]["email"] == email:
+        contact_user_id = candidates[0]["id"]
+    else:
+        contact_user = client.register_user(email)
+        contact_user_id = contact_user["id"]
+
+    return contact_user_id
